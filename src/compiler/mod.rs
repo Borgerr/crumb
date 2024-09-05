cf08691a94815e5b5f9220838a89039138916335
ash <ashkernel02@gmail.com>
2024-09-05 08:51:24 -0600

    error handling

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,20 +1,25 @@
-pub mod lexer;
 use std::{fmt::Display, fs};
 
+pub mod lexer;
 use lexer::tokenize;
+
 pub mod parser;
 use parser::parse;
 use thiserror::Error;
 
+pub mod codegen;
+
 #[derive(Error, Debug)]
 pub enum CompileError {
     Lex { e: lexer::LexError },
+    Parse { e: parser::ParseError },
 }
 
 impl Display for CompileError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Lex { e } => write!(f, "{}", e),
+            Self::Parse { e } => write!(f, "{}", e),
         }
     }
 }
@@ -29,20 +34,23 @@ pub fn compile(input_file: String, l: bool, p: bool, c: bool) -> Result<String, 
     let res = tokenize(source);
     if let Err(e) = res {
         return Err(CompileError::Lex { e });
-    }
-    if l {
+    } else if l {
+        let mut it = res.unwrap().into_iter();
+        while let Some(t) = it.next() {
+            println!("TOKEN!!! {}", t);
+        }
         return Ok(String::from("magic words"));
     }
-    let mut iter = res.clone().unwrap().into_iter();
-    while let Some(t) = iter.next() {
-        println!("TOKEN!!! {}", t);
-    }
-
     let res = parse(res.unwrap());
 
-    match res {
-        Ok(t) => println!("RETURNED VALID ProgramC t = {}", t),
-        Err(e) => println!("RETURNED ERROR e = {}", e),
+    if let Err(e) = res {
+        return Err(CompileError::Parse { e });
+    } else if p {
+        match res {
+            Ok(t) => println!("RETURNED VALID ProgramC t = {}", t),
+            Err(e) => println!("RETURNED ERROR e = {}", e),
+        }
+        return Ok(String::from("magic words"));
     }
 
     Ok(String::from("COMPILE RETURNED WOOHOO!!!"))
